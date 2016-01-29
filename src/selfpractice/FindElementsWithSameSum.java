803c7851ea96d00ef84fb661941d73a47be53e49
Sandeep Kulkarni <sandeep.kul88@gmail.com>
2016-01-29 15:37:56 -0800

    FindElemetsWithSameSum solution
    FindElemetsWithSameSum solution

--- src/selfpractice/FindElementsWithSameSum.java
+++ src/selfpractice/FindElementsWithSameSum.java
@@ -1,5 +1,6 @@
 package selfpractice;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class FindElementsWithSameSum {
@@ -14,13 +15,31 @@ public class FindElementsWithSameSum {
 		}
 		
 		//print arr
+		System.out.println("Input Array :");
 		for(int i=0; i<noOfElements; i++){
-			System.out.println(arr[i]);
+			System.out.print(arr[i] + " ");
 		}
 		
-		//
+		System.out.println("\n"+findElemetsWithSameSum(arr));
 		
 		scanner.close();
+	}
+	
+	private static String findElemetsWithSameSum(int[] arr){
+		HashMap<Integer,String> map = new HashMap<Integer, String>();
+		for(int i=0; i < arr.length; i++){
+			for(int j=i+1; j < arr.length; j++){
+				int sum = arr[i] + arr[j];
+				String pair1 = "("+arr[i]+","+arr[j]+")";
+				if(map.containsKey(sum)){
+					String pair2 = map.get(sum);
+					return pair1 + pair2;
+				}else{
+					map.put(sum, pair1);
+				}
+			}
+		}
+		return "No pairs found";
 	}
 
 }
